84920ea37152a0d6042959e053611b18aa1c4eb6
KarnaukhovKirill <kirya2361@gmail.com>
2021-03-05 10:19:02 +0300

    Исправил код метода Mortgage

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -2,9 +2,8 @@ package ru.job4j.loop;
 
 public class Mortgage {
     public static int year(double amount, double salary, double percent) {
-        int count = 1;
-        amount += amount * percent / 100;
-        while (salary < amount) {
+        int count = 0;
+        while (amount > 0) {
             amount += amount * percent / 100;
             amount = amount - salary;
             count += 1;
