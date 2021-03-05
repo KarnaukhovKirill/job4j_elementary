93303d15cc282f8defaa7fbbf2bd0fec7e3d1459
KarnaukhovKirill <kirya2361@gmail.com>
2021-03-05 14:33:44 +0300

    Работа с массивом

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -3,10 +3,14 @@ package ru.job4j.array;
 public class ArrayDefinition {
     public static void main(String[] args) {
         short[] ages = new short[10];
-        String[] surnames = new String[100500];
+        String[] names = new String[4];
         float[] prices = new float[40];
-        System.out.println("Размер массива ages: " + ages.length);
-        System.out.println("Размер массива surnames: " + surnames.length);
-        System.out.println("Размер массива prices: " + prices.length);
+        names[0] = "Mark Tsukerberg";
+        names[1] = "Nik Baskov";
+        names[2] = "Pavel Durov";
+        names[3] = "Andrei Rublev";
+        for (int i = 0; i <= names.length - 1; i++) {
+            System.out.println(names[i]);
+        }
     }
 }
