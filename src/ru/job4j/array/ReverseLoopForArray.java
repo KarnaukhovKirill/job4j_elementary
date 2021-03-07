11f954858a9c6609c6eb28bb85ed75df9e53f140
KarnaukhovKirill <kirya2361@gmail.com>
2021-03-07 13:46:46 +0300

    Изменил код

--- src/ru/job4j/array/ReverseLoopForArray.java
+++ src/ru/job4j/array/ReverseLoopForArray.java
@@ -5,7 +5,7 @@ public class ReverseLoopForArray {
         int[] numbers = {1, 2, 3, 4, 5, 6, 7};
         for (int i = 0; i < numbers.length; i++) {
             if (i % 2 == 0 && i != 0) {
-                System.out.println(numbers[i]);
+                System.out.println(numbers[numbers.length - 1 - i]);
             }
         }
     }
