767c5bfd510fbb81bd4666c27b9f24932510bfd6
KarnaukhovKirill <kirya2361@gmail.com>
2021-03-07 13:52:50 +0300

    Исправил код

--- src/ru/job4j/array/EndsWith.java
+++ src/ru/job4j/array/EndsWith.java
@@ -2,11 +2,10 @@ package ru.job4j.array;
 
 public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
-        boolean result = false;
+        boolean result = true;
         for (int index = 0; index < post.length; index++) {
-            if (word[word.length - 1 - index] == post[post.length - 1 - index]) {
-                result = true;
-            } else {
+            if (word[word.length - 1 - index] != post[post.length - 1 - index]) {
+                result = false;
                 break;
             }
         }
